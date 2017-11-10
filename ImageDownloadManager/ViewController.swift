7e2471a14c56564c81cda19d7b7ca1e7c0bd8308
jeffmew <jeffmew@gmail.com>
2017-11-10 11:36:43 -0800

    Initial Commit

---
+++ ImageDownloadManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageDownloadManager
+//
+//  Created by Jeff Mew on 2017-11-10.
+//  Copyright © 2017 Jeff Mew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
